4915ca7ea0dd1d04a9327d6dc6945c2d42b6f3ae
tshepo <tshepo.mashiloane@umuzi.org>
2023-02-14 21:10:18 +0200

    barebones

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+  get "about-us", to: "about#index", as: :about
   root to: 'home#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
