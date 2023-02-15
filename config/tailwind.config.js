9dbe9d75ce257d574c11b0b9e52c73caebcee775
tshepo <tshepo.mashiloane@umuzi.org>
2023-02-15 10:58:39 +0200

    bootstrap styling

---
+++ config/tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    content: [
+        "./app/views/layouts/_nav.html.erb",
+        "./public/*.html",
+        "./app/helpers/**/*.rb",
+        "./app/javascript/**/*.js",
+        "./app/views/**/*.{erb,haml,html,slim}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/typography"),
+    ],
+};
